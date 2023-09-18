25832180466cc33509c83460332b4b45434edb8f
Ninamahdawe <ninamahdawe11@gmail.com>
2023-09-18 11:47:34 -0700

    adding the product data in the product routes.js file

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -79,12 +79,11 @@ router.put("/:id", async (req, res) => {
         };
       });
 
-    // figure out which ones to remove
     const productTagsToRemove = productTags
       .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
       .map(({ id }) => id);
     // run both actions
-    return Promise.all([
+    await Promise.all([
       ProductTag.destroy({ where: { id: productTagsToRemove } }),
       ProductTag.bulkCreate(newProductTags),
     ]);
