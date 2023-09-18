90e22b2bc13cc44c9e4ac98f3e23ba2c412482df
Ninamahdawe <ninamahdawe11@gmail.com>
2023-09-17 22:46:09 -0700

    adding the category data in the category routes.js file

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -2,15 +2,34 @@ const router = require('express').Router();
 const { Category, Product } = require('../../../models');
 
 // The `/api/categories` endpoint
-
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
-  // be sure to include its associated Products
+  try {
+    const CategoryData = await Category.findAll({
+      include: [{ model: Product }],
+    }); // be sure to include its associated Products
+    res.status(200).json(CategoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
-
-router.get('/:id', (req, res) => {
+// be sure to include its associated Products
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
