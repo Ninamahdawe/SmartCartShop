1f862e884f1f4bc44d24148b25e846983170f39f
Ninamahdawe <ninamahdawe11@gmail.com>
2023-09-17 22:08:40 -0700

    defining the columns for the category.js file

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-products.belongTo;
+products.belongTo
 // Categories have many Products
 
 // Products belongToMany Tags (through ProductTag)
