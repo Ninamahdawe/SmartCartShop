4cc62f2ca93ab58d240713b01d40fe198d368fa5
Ninamahdawe <ninamahdawe11@gmail.com>
2023-09-18 13:41:37 -0700

    debuggin catrgory routes.js file

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,7 +3,6 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 router.get('/', async (req, res) => {
-  // find all categories
   try {
     const CategoryData = await Category.findAll({
       include: [{ model: Product }],
@@ -13,9 +12,7 @@ router.get('/', async (req, res) => {
     res.status(500).json(err);
   }
 });
-// be sure to include its associated Products
 router.get('/:id', async (req, res) => {
-  // find one category by its `id` value
   try {
     const categoryData = await Category.findByPk(req.params.id, {
       include: [{ model: Product }],
