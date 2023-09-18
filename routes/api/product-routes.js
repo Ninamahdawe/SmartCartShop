32fe25e358f33fa21048dd013358f77b88d127d2
Ninamahdawe <ninamahdawe11@gmail.com>
2023-09-18 14:17:32 -0700

    debuggin catrgory routes.js file

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { Router } = require("express");
-const { Product, Category, Tag, ProductTag } = require("../../../models");
+const { Product, Category, Tag, ProductTag } = require("../../models");
 
 // The `/api/products` endpoint
 // get all products
