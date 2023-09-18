ac00f5a99916e7eb10fb50580fb2880516b50b51
Ninamahdawe <ninamahdawe11@gmail.com>
2023-09-17 22:58:37 -0700

    adding the category data in the category routes.js file

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -43,7 +43,21 @@ router.post('/', async (req, res) => {
 });
 
 // update a category by its `id` value
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedCategoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!updatedCategoryData[0]) {
+      res.status(404).json({ message: 'No updated category with this id!' });
+      return;
+    }
+    res.status(200).json(updatedCategoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 
