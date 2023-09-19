beb36f8f8251f9f2dd480143af4f06e82394337f
Ninamahdawe <ninamahdawe11@gmail.com>
2023-09-18 22:56:31 -0700

    adding comments to categoryroutes.js file section

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
+
+// GET all categories
 router.get('/', async (req, res) => {
   try {
     const CategoryData = await Category.findAll({
